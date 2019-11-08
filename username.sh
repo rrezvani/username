7abeb4b78db5a57f4983a0aaf8b26632e907fb17
Romtin Rezvani <rrezvani@chapman.edu>
2019-11-08 10:50:19 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -3,7 +3,7 @@
 # Romtin Rezvani
 echo "Enter a password using only lowercase letters, digits, and the underscore symbol. Your first character must be a letter and it can be 3-12 characters wrong: "
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[a-z][0-9_]{3-12}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{2-12}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid password! Please try again."
 	echo "Enter another password: "
